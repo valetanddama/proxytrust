c91e70c370711d69123123221a76142efe028c8c
valetanddama <valetanddama@gmail.com>
2018-07-03 16:00:58 +0500

    Create proxy.go

---
+++ proxy.go
@@ -0,0 +1,19 @@
+package proxy
+
+import (
+	"net/http"
+)
+
+func behindProxy(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		remoteAddr := req.RemoteAddr
+		xForwardedFor := req.Header.Get("X-Forwarded-For")
+
+		if xForwardedFor != "" {
+			remoteAddr = xForwardedFor
+		}
+
+		req.RemoteAddr = remoteAddr
+		next.ServeHTTP(res, req)
+	})
+}
