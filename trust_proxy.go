3eed54d9e7f50b4eda63d54f3cf1a6285b967cea
a.lukinykh <a.lukinykh@xsolla.com>
2018-07-04 15:57:09 +0500

    refactoring

--- trust_proxy.go
+++ trust_proxy.go
@@ -8,7 +8,7 @@ import (
 
 func TrustProxyClientIp(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
-		regexIpPattern := "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]).){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
+		regexIpPattern := "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]).){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(:[0-9]+)?$"
 
 		remoteAddr := req.RemoteAddr
 		xForwardedFor := strings.TrimSpace(strings.Split(req.Header.Get("X-Forwarded-For"), ",")[0])
